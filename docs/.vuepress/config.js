1c4dcd8304e4206e6bd4ecb33dc7b6918f98fa12
hcyyoyo <1350181550@qq.com>
2019-10-15 19:03:15 +0800

    构建部署

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,6 @@ module.exports = {
     title: '前端知识仓',
     description: '花里胡哨 ',
     // 默认在.vuepress目录下
-    dest: './dist',
     port: '7777',
     head: [
         ['link', {rel: 'icon', href: '/img/icon.png'}],
