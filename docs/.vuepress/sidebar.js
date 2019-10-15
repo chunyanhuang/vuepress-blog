563a25ca8e8b4655d83cffe709236625718a0da6
hcyyoyo <1350181550@qq.com>
2019-10-15 21:09:51 +0800

    导航栏初始化

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -4,4 +4,7 @@ module.exports = {
 
 	'/baodian/zero': require('../baodian/zero/sidebar'),
 	'/baodian/high': require('../baodian/high/sidebar'),
+
+	'/suanfa/one': require('../suanfa/one/sidebar'),
+	'/suanfa/two': require('../suanfa/two/sidebar')
 }
