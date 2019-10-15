93431e0a4b23acbc9bddfdfe16e82790c943bbdb
hcyyoyo <1350181550@qq.com>
2019-10-15 16:22:35 +0800

    构建部署

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 ### 如果你想要部署到 https://USERNAME.github.io
-git push -f https://${GITHUB_TOKEN}@github.com:chunyanhuang/chunyanhuang.github.io.git master
+### git push -f https://${GITHUB_TOKEN}@github.com:chunyanhuang/chunyanhuang.github.io.git master
 
 ### 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
-### git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
+git push -f git@github.com:chunyanhuang/vuepress-blog.git master:gh-pages
 
 cd -
