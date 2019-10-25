36e7d19684400123736516716debec746ad2a8ab
hcyyoyo <1350181550@qq.com>
2019-10-25 14:38:22 +0800

    vuepress坑

--- docs/baodian/zero/README.md
+++ docs/baodian/zero/README.md
@@ -1,5 +1,7 @@
-基础知识的readme
+### 基础知识
 
++ [js基础知识](./notes/js基础知识)
++ [vuepress使用坑](./notes/vuepress使用坑)
 
 
 
--- docs/baodian/zero/sidebar.js
+++ docs/baodian/zero/sidebar.js
@@ -4,6 +4,7 @@ module.exports = [
 		  collapsable: true,
 		  children:[
 			'/baodian/zero/notes/js基础知识',
+			'/baodian/zero/notes/vuepress使用坑',
 		  ]
 		}
 	]
