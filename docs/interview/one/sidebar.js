9b473b5a15aa1fb729ce120e984cc099121e06ae
hcyyoyo <1350181550@qq.com>
2019-10-25 14:22:18 +0800

    数据类型

--- docs/interview/one/sidebar.js
+++ docs/interview/one/sidebar.js
@@ -3,8 +3,8 @@ module.exports = [
 		  title:'js基础知识',
 		  collapsable: true,
 		  children:[
-			'/interview/one/notes/数据类型',
 			'/interview/one/notes/数组',
+			'/interview/one/notes/数据类型'
 		  ]
 		}
 	]
