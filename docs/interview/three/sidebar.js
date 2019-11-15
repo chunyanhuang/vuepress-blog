89bbc7fc7e675a5e9ee35263031668045759b339
hcyyoyo <1350181550@qq.com>
2019-11-15 21:37:56 +0800

    垂直居中

--- docs/interview/three/sidebar.js
+++ docs/interview/three/sidebar.js
@@ -3,7 +3,8 @@ module.exports = [
 		  title:'html+css',
 		  collapsable: true,
 		  children:[
-			'/interview/three/notes/html+css',
+			'/interview/three/notes/水平垂直居中',
+			'/interview/three/notes/左右固定中间自适应',
 		  ]
 		}
 	]
